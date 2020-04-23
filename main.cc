ed4b1968bea5326a506d57b50b6db2eb5fc8237c
nibing <248918860@qq.com>
2020-04-23 20:24:16 +0800

    add main.cc

---
+++ main.cc
@@ -0,0 +1,13 @@
+ /// @file    main.cc
+ /// @author  daxiaobing(248918860@qq.com)
+ /// @date    2020-04-23 20:22:41
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "hello,world!" << endl;
+	return 0;
+}
